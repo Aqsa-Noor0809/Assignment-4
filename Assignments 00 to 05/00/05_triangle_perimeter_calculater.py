744b9f46ff7fc1166c92614b6b200d15cd386e07
Aqsa Noor <aqsanoor0809@gmail.com>
2025-04-17 11:11:16 +0500

    Initial Commit

--- Assignments 00 to 05/00/05_triangle_perimeter_calculater.py
+++ Assignments 00 to 05/00/05_triangle_perimeter_calculater.py
@@ -1,12 +1,9 @@
 def main():
     print("Write the length of thrice sides below!")
-    len1 = input("Enter the length of First side: ")
-    length1 : int = int(len1)
-    len2 = input("Enter the length of Second side: ")
-    length2 : int = int(len2)
-    len3 = input("Enter the length of Third side: ")
-    length3 : int = int(len3)
-    result : int = length1 + length2 + length3
+    len1 = float(input("Enter the length of First side: "))
+    len2 = float(input("Enter the length of Second side: "))
+    len3 = float(input("Enter the length of Third side: "))
+    result : int = len1 + len2 + len3
     print(f"\nThe perimeter of the Triangle is \033[1;3m{result}\033[0m . \n")
 
 if __name__ == '__main__':
